d0765e399b0076b2b3881ee19d429702207c08b8
Guilhem Lettron <g.lettron@criteo.com>
2016-10-31 18:13:54 +0100

    Add issues_url and source_url

--- metadata.rb
+++ metadata.rb
@@ -6,13 +6,8 @@ description 'Installs/Configures docker-engine'
 long_description 'Installs/Configures docker-engine'
 version '0.1.0'
 
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Issues` link
-# issues_url 'https://github.com/<insert_org_here>/docker-engine/issues' if respond_to?(:issues_url)
-
-# If you upload to Supermarket you should set this so your cookbook
-# gets a `View Source` link
-# source_url 'https://github.com/<insert_org_here>/docker-engine' if respond_to?(:source_url)
+issues_url 'https://github.com/redguide/docker-engine/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/redguide/docker-engine' if respond_to?(:source_url)
 
 supports 'ubuntu'
 supports 'debian'
