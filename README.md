194e1acc2c59054dd272eacaefe3d48cfd6ab717
Guilhem Lettron <g.lettron@criteo.com>
2016-10-31 11:55:03 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# docker-engine
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'docker-engine'
+maintainer 'The Authors'
+maintainer_email 'guilhem@lettron.fr'
+license 'apachev2'
+description 'Installs/Configures docker-engine'
+long_description 'Installs/Configures docker-engine'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/docker-engine/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/docker-engine' if respond_to?(:source_url)
