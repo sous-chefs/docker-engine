95567b0b5977bb011be856684225ee693f29ca76
Guilhem Lettron <g.lettron@criteo.com>
2016-12-19 17:25:07 +0100

    Release 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'guilhem@lettron.fr'
 license 'apachev2'
 description 'Installs/Configures docker-engine'
 long_description 'Installs/Configures docker-engine'
-version '0.1.0'
+version '0.2.0'
 
 issues_url 'https://github.com/redguide/docker-engine/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/redguide/docker-engine' if respond_to?(:source_url)
