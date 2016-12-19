de8cd62dd72c858748d2aaae9516685dab9516c9
Guilhem Lettron <g.lettron@criteo.com>
2016-12-19 17:30:58 +0100

    Fix chef_version with dpl and Release 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'guilhem@lettron.fr'
 license 'apachev2'
 description 'Installs/Configures docker-engine'
 long_description 'Installs/Configures docker-engine'
-version '0.2.0'
+version '0.2.1'
 
 issues_url 'https://github.com/redguide/docker-engine/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/redguide/docker-engine' if respond_to?(:source_url)
 
-chef_version '>= 12.14'
+chef_version '>= 12.14' if respond_to?(:chef_version)
 
 supports 'ubuntu'
 supports 'centos'
